214f4e3ad57d4e534d46d7d0be106f20c73f599f
Alexey Sharov <alexey.sharov@team.wrike.com>
2018-04-23 14:59:42 +0300

    initial

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:pdf_optimizer/pdf_optimizer.dart' as pdf_optimizer;
+
+main(List<String> arguments) {
+  print('Hello world: ${pdf_optimizer.calculate()}!');
+}
---
+++ test/pdf_optimizer_test.dart
@@ -0,0 +1,8 @@
+import 'package:pdf_optimizer/pdf_optimizer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
